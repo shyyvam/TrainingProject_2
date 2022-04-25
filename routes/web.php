1931c397599d56a70efadbccae69bc6cedf814cc
SHIVAM SHARMA <51492085+shyyvam@users.noreply.github.com>
2022-04-25 23:52:34 +0530

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -10,50 +10,54 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+         //***START***
+  //Route::get('/','HomeController@show');
+  Auth::routes();
+  Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-
-
-
+                    //**ADMIN**
 Route::group(['middleware'=>['auth','admin']], function(){
-  Route::get('/dashboard', function () {
-      return view('admin.dashboard');
-  });
-  Route::get('/role-register','Admin\DashboardController@registered');
-  Route::get('/role-edit/{id}','Admin\DashboardController@registerEdit');
-  Route::put('role-register-update/{id}','Admin\DashboardController@registerUpdate');
-  Route::delete('/role-delete/{id}','Admin\DashboardController@registerDelete');
-  Route::get('/abouts','Admin\AboutusController@index');
-  Route::get('/books','Admin\BooksController@index');
-  Route::post('/save-books','Admin\BooksController@save');
-  Route::get('/booksedit/{book_id}','Admin\BooksController@edit');
-  Route::put('/books-update/{book_id}','Admin\BooksController@update');
-  Route::delete('/books-delete/{book_id}','Admin\BooksController@delete');
-  Route::get('/search','Admin\BooksController@search');
-  Route::get('/fine','IssueController@fine');
+                    //**ADMIN-DASHBOARD**
+     Route::get('/','Admin\DashboardController@show');
+     Route::get('/dashboard','Admin\DashboardController@show');
 
+                    //**ADMIN CRUD**
+     Route::get('/role-register','Admin\DashboardController@registered');
+     Route::get('/role-edit/{id}','Admin\DashboardController@registerEdit');
+     Route::put('role-register-update/{id}','Admin\DashboardController@registerUpdate');
+     Route::delete('/role-delete/{id}','Admin\DashboardController@registerDelete');
 
+                    //**ABOUT US PAGE**
+     Route::get('/abouts','Admin\AboutusController@index');
+
+                    //**BOOKS CRUD**
+     Route::get('/books','Admin\BooksController@index');
+     Route::post('/save-books','Admin\BooksController@save');
+     Route::get('/booksedit/{book_id}','Admin\BooksController@edit');
+     Route::put('/books-update/{book_id}','Admin\BooksController@update');
+     Route::delete('/books-delete/{book_id}','Admin\BooksController@delete');
+     Route::get('/search','Admin\BooksController@search');
+
+                    //**FINE DETAILS**
+     Route::get('/fine','IssueController@fine');
 });
 
-Route::group(['middleware'=>['auth','student']], function(){
-  Route::get('/student-dashboard', function () {
-      return view('nonadmin.dashboard');
-  });
-  Route::get('/studentprofile/{id}','Nonadmin\DashboardController@index');
-  Route::put('/student-profile-update/{id}','Nonadmin\DashboardController@profileUpdate');
-  Route::get('/studentbooks','IssueController@studentIndex');
-  Route::get('/issueupdate/{book_id}','IssueController@issue_Update');
-  Route::get('/reissueupdate/{id}/{book_id}','IssueController@reissue_Update');
-  Route::get('/finebooks','IssueController@fineBooks');
-  Route::delete('/return/{id}/{book_id}','IssueController@return');
-  
 
+                  //**STUDENT**
+Route::group(['middleware'=>['auth','student']], function(){
+                  //**DASHBOARD**
+     Route::get('/student-dashboard','Nonadmin\DashboardController@show');
+
+                  //**USER-PROFILE**
+     Route::get('/studentprofile/{id}','Nonadmin\DashboardController@index');
+     Route::put('/student-profile-update/{id}','Nonadmin\DashboardController@profileUpdate');
+
+                  //**BOOKS-ISSUE**
+     Route::get('/studentbooks','IssueController@studentIndex');
+     Route::get('/issueupdate/{book_id}','Nonadmin\DashboardController@issueBook');
+
+                  //**BOOKS-REISSUE-RETURN**
+     Route::get('/reissueupdate/{id}/{book_id}','Nonadmin\DashboardController@reissueBook');
+     Route::get('/finebooks','IssueController@fineBooks');
+     Route::delete('/return/{id}/{book_id}','IssueController@return');
 });
