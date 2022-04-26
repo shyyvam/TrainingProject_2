6eb5d0c9556e44491cc3967b16b87669ac778893
SHIVAM SHARMA <51492085+shyyvam@users.noreply.github.com>
2022-04-26 11:23:57 +0530

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DummpyAPI;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,3 +17,4 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get("data",[DummpyAPI::class,'getData']);
