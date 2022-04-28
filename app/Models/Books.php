b2e64799b708620439d84736a38faffe65826944
SHIVAM SHARMA <51492085+shyyvam@users.noreply.github.com>
2022-04-28 17:39:59 +0530

    Add files via upload

--- app/Models/Books.php
+++ app/Models/Books.php
@@ -16,97 +16,70 @@ class Books extends Model
                                  'book_version',
                                  'book_subject'];
     protected $primaryKey     = 'book_id';
+    public static $paginateValue     = 5;
 
     public static function deleteBooks($book_id)
     {
-      $books                  = Books::findOrFail($book_id);
+      $books = Books::findOrFail($book_id);
       return $books->delete();
     }
 
     public static function saveBooks($request)
     {
-      $validator = Validator::make($request -> all(),
-         [
-           'title'   => 'required|max:191|min:3',
-           'author'  => 'required|max:191|min:3',
-           'version' => 'required|max:191|numeric|min:1',
-           'subject' => 'required|max:191|min:3',
-         ]
-       );
-
-       if($validator -> fails())
-       {
-         return redirect('/books')
-               -> withErrors($validator)
-               -> withInput()
-               -> with('status','Data not Added for Books');
-       }
-       else
-       {
           $books                 = new Books;
           $books->book_name      = $request->input('title');
           $books->book_author    = $request->input('author');
           $books->book_version   = $request->input('version');
           $books->book_subject   = $request->input('subject');
           $books->save();
+
           return redirect('/books')
                 ->with('status','Data Added for Books');
-      }
     }
+
     public static function editBooks($book_id)
     {
-      $books                 = Books::findOrFail($book_id);
+      $books = Books::findOrFail($book_id);
       return $books;
     }
+
     public static function updateBooks($request,$book_id)
     {
-      $validator = Validator::make($request -> all(),
-         [
-           'title'   => 'required|max:191|min:3',
-           'author'  => 'required|max:191|min:3',
-           'version' => 'required|max:191|numeric|min:1',
-           'subject' => 'required|max:191|min:3',
-         ]
-       );
-
-       if($validator -> fails())
-       {
-         return redirect('/books')
-               -> withErrors($validator)
-               -> withInput()
-               -> with('status','Data not Updated for Books');
-       }
-       else
-       {
           $books                = Books::findOrFail($book_id);
           $books->book_name     = $request->input('title');
           $books->book_author   = $request->input('author');
           $books->book_version  = $request->input('version');
           $books->book_subject  = $request->input('subject');
           $books->update();
+
           return redirect('/booksedit/'.$book_id)
                 ->with('status','Data Updated for Books');
-       }
     }
-    public static function updateRecord($book_id) {
-        $book               = Books::findOrFail($book_id);
-        $book->status       = 'y';
+
+    public static function updateRecord($book_id)
+    {
+        $book          = Books::findOrFail($book_id);
+        $book->status  = 'y';
+
         return $book->save();
     }
 
-    public static function updateIssue($book_id) {
-        $book               = Books::findOrFail($book_id);
-        $book->status       = 'n';
+    public static function updateIssue($book_id)
+    {
+        $book          = Books::findOrFail($book_id);
+        $book->status  = 'n';
+
         return $book->save();
     }
+
     public static function searchBooks($request)
     {
+      $search   = $request->get('search');
+      $books    = DB::table('books')
+                ->where('book_name','like','%'.$search.'%')
+                ->orWhere('book_subject','like','%'.$search.'%')
+                ->paginate(self::$paginateValue);
 
-      $search               = $request->get('search');
-      $books                = DB::table('books')
-                            ->where('book_name','like','%'.$search.'%')
-                            ->orWhere('book_subject','like','%'.$search.'%')
-                            ->paginate(5);
       return $books;
     }
 
--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -18,31 +18,31 @@ class Issue extends Model
 {
   protected $table        = "issue";
   protected $primaryKey   = "id";
-  protected $fillable     = ["issue_date","return_date","b_id","u_id"];
+  protected $fillable     = ["issue_date",
+                             "return_date",
+                             "b_id","u_id"];
 
   public static function studentIndex()
   {
-    $books                = Books::all();
-    $users                = User::all();
-    $issue                = Issue::all();
-    $data                 = DB::table('books')
-                                ->leftjoin('issue','books.book_id','=','issue.b_id')
-                                ->select('books.book_id','books.book_name','issue.id')
-                                ->where('books.status', '=', 'n')
-                                ->get();
-    return $data;
-  }
-  public static function adminFine()
-  {
-    $books                 = Books::all();
-    $users                 = User::all();
-    $data                  =  Issue::join('users','issue.u_id','=','users.id')
-                                    ->join('books','issue.b_id','=','books.book_id')
-                                    ->select('issue.id','issue.fine','books.book_name','users.phone_number','users.email','issue.issue_date','issue.return_date')
-                                    ->get();
+    $data   = DB::table('books')
+                ->leftjoin('issue','books.book_id','=','issue.b_id')
+                ->select('books.book_id','books.book_name','issue.id')
+                ->where('books.status', '=', 'n')
+                ->get();
 
     return $data;
   }
+
+  public static function adminFine()
+  {
+    $data  =  Issue::join('users','issue.u_id','=','users.id')
+                   ->join('books','issue.b_id','=','books.book_id')
+                   ->select('issue.id','issue.fine','books.book_name','users.phone_number','users.email','issue.issue_date','issue.return_date')
+                   ->get();
+
+    return $data;
+  }
+
   public static function createIssue($book_id) {
       //$this->validate();
       $issue              = new self();
@@ -52,13 +52,17 @@ class Issue extends Model
       $issue->return_date = $return_date;
       $issue->b_id        = $book_id;
       $issue->u_id        = Auth::user()->id;
+
       return $issue->save();
   }
-  public static function createReissue($book_id) {
+
+  public static function createReissue($book_id)
+  {
     $issue                = Issue::findOrFail($book_id);
     $date                 = date("d.m.y");
     $return_date          = strtotime($date.'+7 days');
     $issue->return_date   = $return_date;
+
     return $issue->save();
   }
   /*private function validate($data) {
@@ -70,23 +74,21 @@ class Issue extends Model
           }
       }
   }*/
+
   public static function fineBooks()
   {
-    $books                = Books::all();
-    $users                = User::all();
-    $issue                = Issue::all();
-    $data                 = Issue::join('users','issue.u_id','=','users.id')
-                                  ->join('books','issue.b_id','=','books.book_id')
-                                  ->select('issue.id','books.book_id','issue.fine','books.book_name','users.phone_number','users.email','issue.issue_date','issue.return_date')
-                                  ->where('users.id','=',Auth::user()->id)
-                                  ->get();
+    $data  = Issue::join('users','issue.u_id','=','users.id')
+                  ->join('books','issue.b_id','=','books.book_id')
+                  ->select('issue.id','books.book_id','issue.fine','books.book_name','users.phone_number','users.email','issue.issue_date','issue.return_date')
+                  ->where('users.id','=',Auth::user()->id)
+                  ->get();
 
       return $data;
   }
 
   public static function deleteIssue($id)
   {
-    $issue                = Issue::findOrFail($id);
+    $issue = Issue::findOrFail($id);
     return $issue->delete();
   }
 
